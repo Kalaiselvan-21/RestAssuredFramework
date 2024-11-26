3c3402f537e9128581f8422462137b4a7d240a3b
Kalaiselvan-21 <kalai21.it@gmail.com>
2024-11-26 13:56:33 +0530

    JSONschemaValidation

--- RestAssuredFramework/src/test/java/schemaValidation/JSONschemaValidation.java
+++ RestAssuredFramework/src/test/java/schemaValidation/JSONschemaValidation.java
@@ -1,9 +1,6 @@
 package schemaValidation;
 
 import static io.restassured.RestAssured.*;
-import static io.restassured.matcher.RestAssuredMatchers.*;
-import static org.hamcrest.Matchers.*;
-
 import org.testng.annotations.Test;
 
 import io.restassured.module.jsv.JsonSchemaValidator;
